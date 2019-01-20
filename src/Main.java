70e6fe973c88ab0acaba65010e78d8f269367819
Luke <zhongwei.li@mavs.uta.edu>
2019-01-20 13:27:49 -0600

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
+    //Version -1
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Git!");
+    }
+
+    private void foo() {
+        int i = 0;
+        int j = 1;
+
+        System.out.println(i+j);
     }
 }
